8cf49d0f36d4f89536011f5a16330b5bf5b82567
Quang <boncoi19990@gmail.com>
2023-11-24 09:09:40 +0700

    add onedark theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,12 @@
+
 function ColorMyPencils(color)
 	-- color = color or "rose-pine"
-	color = color or "tokyonight-night"
+	color = color or "onedark"
+	require('onedark').setup{
+		style = 'darker',
+		toggle_style_key = '<leader>cl', -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+		toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+	}
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none"  })
