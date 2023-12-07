14bb10e60f7ee39f024a002006d9f8abb26b2de2
Quang <boncoi19990@gmail.com>
2023-12-07 14:26:42 +0700

    add toggle search hightlight

--- lua/eanigmal/remap.lua
+++ lua/eanigmal/remap.lua
@@ -67,9 +67,3 @@ local function toggleHightlight()
 end
 vim.keymap.set({"n", "v"}, "<leader>n", toggleHightlight, opts)
 
--- terminal
-vim.keymap.set({"n", "v"}, "<leader>t", "<C-w>v:ter<CR><S-a>", opts)
-vim.keymap.set("t", "<C-n>", "<C-\\><C-n>", opts)
-vim.keymap.set("t", "<C-w>", "<C-\\><C-n><C-w>", opts)
-vim.keymap.set({"n", "v", "t"}, "<Tab>", "gt", opts)
-
