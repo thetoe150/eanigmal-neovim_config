9d1dda4c61c809df70d77b8f8ec4d97dca31007f
eanigmal <boncoi19990@gmail.com>
2023-12-09 19:02:42 +0700

    set terminal to PS && hlsearch=false

--- lua/eanigmal/set.lua
+++ lua/eanigmal/set.lua
@@ -10,19 +10,30 @@ vim.opt.expandtab = false
 
 vim.opt.wrap = false
 
-vim.opt.hlsearch = true
+vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- terminal
 vim.opt.termguicolors = true
+vim.cmd("set shell=powershell")
+vim.cmd("set shellcmdflag=-command")
+vim.cmd("set shellquote=\"")
+vim.cmd("set shellxquote=")
+-- let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command '
+-- let &shellquote   = ''
+-- let &shellpipe    = '| Out-File -Encoding UTF8 %s'
+-- let &shellredir   = '| Out-File -Encoding UTF8 %s'
+
+-- fold
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.api.nvim_cmd({cmd = "set", args = {"nofoldenable"}},{})
 
 vim.opt.scrolloff = 2
 vim.opt.signcolumn = "yes"
 
 vim.opt.colorcolumn = "96"
 
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.api.nvim_cmd({cmd = "set", args = {"nofoldenable"}},{})
 
 vim.g.mapleader = " "
 
